50cc9dfaa6e8b19b8569b2ab3f619131b71811e2
code-with-ali <codewithali567@gmail.com>
2024-11-21 16:37:12 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import Herosection from "../app/component/Herosection"
-import Mainsection from "../app/component/Mainsection"
-import Course from "../app/component/Course"
-import Advancecourse from "../app/component/Advancecourse"
-
-
+import React from 'react';
+import Head from 'next/head';
+import Herosection from '../app/component/Herosection';
+import Mainsection from '../app/component/Mainsection';
+import Course from '../app/component/Course';
+import Advancecourse from '../app/component/Advancecourse';
 
 export default function Page() {
   return (
-    <head>
-     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3139298744644114"
-     crossorigin="anonymous"></script>
-    </head>
-    <div>
-      <Herosection />
-      <Mainsection />
-      <Course />
-      <Advancecourse />
-    </div>
-  )
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3139298744644114"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <div>
+        <Herosection />
+        <Mainsection />
+        <Course />
+        <Advancecourse />
+      </div>
+    </>
+  );
 }
+
